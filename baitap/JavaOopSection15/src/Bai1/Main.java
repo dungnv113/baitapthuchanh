d9237d302675375eaa81b4272e4dca2bff3c3a72
dungnv113 <surivato98@gmail.com>
2022-03-18 17:50:22 +0700

    cm

---
+++ baitap/JavaOopSection15/src/Bai1/Main.java
@@ -0,0 +1,15 @@
+package Bai1;
+
+public class Main {
+
+	// TODO Auto-generated method stub
+	public static void main(String[] args) {
+
+		Person ps1 = new Person(8724, "Huy", 25);
+
+		Person ps2 = new Person(8712, "Tien", 25);
+
+		System.out.println(ps1.equals(ps2));
+	}
+
+}
